9c26e0dc3360e6985159d22d05f84a7075b90271
Adonijah01 <adonijahsingoei@gmail.com>
2023-05-26 16:18:04 +0300

    Adonijah Shell Project

--- MAIN.h
+++ MAIN.h
@@ -1,26 +1,24 @@
-#ifndef SIMPLE_SHELL
-#define SIMPLE_SHELL
+#ifndef SIMPLESHELL
+#define SIMPLESHELL
 
 #define _GNU_SOURCE
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <stdarg.h>
 #include <errno.h>
+#include <stdarg.h>
 
 
 extern char **environ;
 /**
- * struct is equivalent to builtins
- * @b_name - name of the command
- * @b_func - pointer to function.
+* struct builtin - builtins
+* @b_name: name of the command
+* @b_func: pointer to a function
 */
-
-
 typedef struct builtin
 {
 	char *b_name;
@@ -28,19 +26,11 @@ typedef struct builtin
 } builtin_struct;
 
 int (*find_builtins(char *user_input))();
-
+int exit_func(void);
 int env_func(void);
 void change_equal_sig(char *str);
 char *ret_path_line();
 void error_msg(char **args);
-int exit_func(void);
-int execo(char *command, char **args);
-void free_all(char **dptr1, char **dptr2, char *sptr1, char *sptr2);
-void logo(void);
-int strncomparer(char *s1, char *s2, size_t n);
-int strlarge(char *s);
-
-
 char **get_path(char *args);
 char *_insert_path(char **args, char **path);
 char **getenvpath(void);
@@ -49,38 +39,30 @@ int non_interactive_mode(char **av);
 int _interactive(char **av __attribute__((unused)));
 int necklace_pearls(char *buffer);
 char **parsing(char *buffer, int characters);
-
-
-
+int execo(char *command, char **args);
+void free_all(char **dptr1, char **dptr2, char *sptr1, char *sptr2);
+void logo(void);
+int strncomparer(char *s1, char *s2, size_t n);
+int strlarge(char *s);
 char *strduplicate(char *s);
 int strcomparer(char *s1, char *s2);
 char *strconk(char *dest, char *src);
-
-
 int spaces_buster(char *buffer);
 int lennum(int n);
 char *int_to_charac(int num);
 char *_strcat(char *dest, char *src);
-
-char *_strcpy(char *dest, char *src);
-
-void error_badcommand(char **args, char *buffer);
-
 char *_strdup(char *s);
 char *_itoa(int num);
-
-
 char *_strstr(char *x, char *y);
 int _compare(char *x, char *y);
 int _strlen(char *s);
-
-
 int _strcmp(char *s1, char *s2);
 int _strncmp(char *s1, char *s2, size_t n);
+char *_strcpy(char *dest, char *src);
 
-
+void error_badcommand(char **args, char *buffer);
 char **args_constructor(char *buffer);
 void validagetline(char *buffer, int len, char **args, char **env_args);
 int notty(char **av __attribute__((unused)));
-#endif /*SIMPLE_SHELL*/
-<Adonijah Kiplimo>
+#endif /*SIMPLESHELL*/
+
---
+++ errors.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * error_msg - writes error depending on the error number
+ * @args: given command to execute
+ * Return: Nothing
+*/
+void error_msg(char **args)
+{
+	int loop = 1;
+	char *hsh_home = "";
+	char *error_message = malloc(sizeof(char) * 255);
+	char *looper = NULL;
+
+	looper = int_to_charac(loop);
+	error_message = strduplicate(hsh_home);
+	error_message = strconk(error_message, ": ");
+	error_message = strconk(error_message, looper);
+	error_message = strconk(error_message, ": ");
+	error_message = strconk(error_message, args[0]);
+	perror(error_message);
+	free(error_message);
+}
+
+/**
+ * error_badcommand - writes error depending on the error number
+ * @args: given command to execute
+ * @buffer: given command to execute
+ * Return: Nothing
+*/
+void error_badcommand(char **args, char *buffer)
+{
+	write(STDOUT_FILENO, "command not found\n", 18);
+	freedom(1, buffer);
+	buffer = NULL;
+	freedom(2, args);
+	args = NULL;
+}

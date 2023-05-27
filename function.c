f5e2eabce06a6e26ac6b8a98e68cabaff9d99d47
Adonijah01 <adnisdevelopers@gmail.com>
2023-05-27 20:17:05 +0000

    My shell

--- function.c
+++ function.c
@@ -1,65 +1,31 @@
 #include <stdio.h>
 
-/**
- * _compare - compares two strings at a given position
- * @x: string to be compared
- * @y: string to compare
- * Return: Nohing
-*/
-int _compare(char *x, char *y)
+#define MAX_NUM 100
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+void print_point(Point *p)
 {
-	while (*x && *y)
-	{
-		if (*x != *y)
-			return (0);
-
-		x++;
-		y++;
-	}
-
-	return (*y == '\0');
+    printf("Point coordinates: (%d, %d)\n", p->x, p->y);
 }
 
-/**
- * _strstr - compares two strings at a given position
- * @x: string to be compared
- * @y: string to compare
- * Return: pointer to the first occurence
-*/
-char *_strstr(char *x, char *y)
+int main(void)
 {
-	while (*x != '\0')
-	{
-		if ((*x == *y) && _compare(x, y))
-			return (x);
-		x++;
-	}
+    int i;
+    Point points[MAX_NUM];
 
-	return (NULL);
+    for (i = 0; i < MAX_NUM; i++) {
+        points[i].x = i;
+        points[i].y = i * i;
+    }
+
+    for (i = 0; i < MAX_NUM; i++) {
+        print_point(&points[i]);
+    }
+
+    return 0;
 }
 
-
-/**
- * _strcpy - unction that copies the string pointed to by src
- *@dest: char
- *@src:_ char
- * Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0, j;
-
-	while (src[i] != '\0')
-	{
-		i++;
-	}
-	for (j = 0; j <= i; j++)
-	{
-		dest[j] = src[j];
-	}
-	return (dest);
-}
-
-/**
- * Adonijah Kiplimo
- */
